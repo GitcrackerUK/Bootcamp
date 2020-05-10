6a22d0a828d0c65e0896b9aa6430c4d7bfbd8487
gitcrackerUK <pfsiwek@gmail.com>
2020-05-10 21:10:46 +0100

    lol

--- main.js
+++ main.js
@@ -753,67 +753,67 @@
 // console.log(results);
 
 
-const books = [{
-		title: 'Good Omens',
-		authors: ['Terry Pratchett', 'Neil Gaiman'],
-		rating: 4.25,
-		genres: ['fiction', 'fantasy']
-	},
-	{
-		title: 'Changing My Mind',
-		authors: ['Zadie Smith'],
-		rating: 3.83,
-		genres: ['nonfiction', 'essays']
-	},
-	{
-		title: 'Bone: The Complete Edition',
-		authors: ['Jeff Smith'],
-		rating: 4.42,
-		genres: ['fiction', 'graphic novel', 'fantasy']
-	},
-	{
-		title: 'American Gods',
-		authors: ['Neil Gaiman'],
-		rating: 4.11,
-		genres: ['fiction', 'fantasy']
-	},
-	{
-		title: 'A Gentleman in Moscow',
-		authors: ['Amor Towles'],
-		rating: 4.36,
-		genres: ['fiction', 'historical fiction']
-	},
-	{
-		title: 'The Name of the Wind',
-		authors: ['Patrick Rothfuss'],
-		rating: 4.54,
-		genres: ['fiction', 'fantasy']
-	},
-	{
-		title: 'The Overstory',
-		authors: ['Richard Powers'],
-		rating: 4.19,
-		genres: ['fiction', 'short stories']
-	},
-	{
-		title: 'A Truly Horrible Book',
-		authors: ['Xavier Time'],
-		rating: 2.18,
-		genres: ['fiction', 'garbage']
-	},
-	{
-		title: 'The Way of Kings',
-		authors: ['Brandon Sanderson'],
-		rating: 4.65,
-		genres: ['fantasy', 'epic']
-	},
-	{
-		title: 'Lord of the flies',
-		authors: ['William Golding'],
-		rating: 3.67,
-		genres: ['fiction']
-	}
-]
+// const books = [{
+// 		title: 'Good Omens',
+// 		authors: ['Terry Pratchett', 'Neil Gaiman'],
+// 		rating: 4.25,
+// 		genres: ['fiction', 'fantasy']
+// 	},
+// 	{
+// 		title: 'Changing My Mind',
+// 		authors: ['Zadie Smith'],
+// 		rating: 3.83,
+// 		genres: ['nonfiction', 'essays']
+// 	},
+// 	{
+// 		title: 'Bone: The Complete Edition',
+// 		authors: ['Jeff Smith'],
+// 		rating: 4.42,
+// 		genres: ['fiction', 'graphic novel', 'fantasy']
+// 	},
+// 	{
+// 		title: 'American Gods',
+// 		authors: ['Neil Gaiman'],
+// 		rating: 4.11,
+// 		genres: ['fiction', 'fantasy']
+// 	},
+// 	{
+// 		title: 'A Gentleman in Moscow',
+// 		authors: ['Amor Towles'],
+// 		rating: 4.36,
+// 		genres: ['fiction', 'historical fiction']
+// 	},
+// 	{
+// 		title: 'The Name of the Wind',
+// 		authors: ['Patrick Rothfuss'],
+// 		rating: 4.54,
+// 		genres: ['fiction', 'fantasy']
+// 	},
+// 	{
+// 		title: 'The Overstory',
+// 		authors: ['Richard Powers'],
+// 		rating: 4.19,
+// 		genres: ['fiction', 'short stories']
+// 	},
+// 	{
+// 		title: 'A Truly Horrible Book',
+// 		authors: ['Xavier Time'],
+// 		rating: 2.18,
+// 		genres: ['fiction', 'garbage']
+// 	},
+// 	{
+// 		title: 'The Way of Kings',
+// 		authors: ['Brandon Sanderson'],
+// 		rating: 4.65,
+// 		genres: ['fantasy', 'epic']
+// 	},
+// 	{
+// 		title: 'Lord of the flies',
+// 		authors: ['William Golding'],
+// 		rating: 3.67,
+// 		genres: ['fiction']
+// 	}
+// ]
 
 // let bookRating = books.reduce((groupedBooks, book) => {
 // 	const key = Math.floor(book.rating);
@@ -833,7 +833,7 @@ const books = [{
 // }, {});
 
 
-console.log(rest);
+// console.log(rest);
 
 //genres
 
