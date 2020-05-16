fb8b5270ee50f2200ff142a8da606f48dbedac70
gitcrackerUK <pfsiwek@gmail.com>
2020-05-16 18:24:19 +0100

    "look for froot"  moved from DOM to Master

--- main.js
+++ main.js
@@ -1631,3 +1631,65 @@
 
 //     }
 // }
+
+// function SubtractSum(n) {
+//     let sortedFruits = []
+//     let FoundNumber = 0;
+
+//     let fruits = "1 - kiwi 2 - pear 3 - kiwi 4 - banana 5 - melon 6 - banana 7 - melon 8 - pineapple 9 - apple 10 - pineapple 11 - cucumber 12 - pineapple 13 - cucumber 14 - orange 15 - grape 16 - orange 17 - grape 18 - apple 19 - grape 20 - cherry 21 - pear 22 - cherry 23 - pear 24 - kiwi 25 - banana 26 - kiwi 27 - apple 28 - melon 29 - banana 30 - melon 31 - pineapple 32 - melon 33 - pineapple 34 - cucumber 35 - orange 36 - apple 37 - orange 38 - grape 39 - orange 40 - grape 41 - cherry 42 - pear 43 - cherry 44 - pear 45 - apple 46 - pear 47 - kiwi 48 - banana 49 - kiwi 50 - banana 51 - melon 52 - pineapple 53 - melon 54 - apple 55 - cucumber 56 - pineapple 57 - cucumber 58 - orange 59 - cucumber 60 - orange 61 - grape 62 - cherry 63 - apple 64 - cherry 65 - pear 66 - cherry 67 - pear 68 - kiwi 69 - pear 70 - kiwi 71 - banana 72 - apple 73 - banana 74 - melon 75 - pineapple 76 - melon 77 - pineapple 78 - cucumber 79 - pineapple 80 - cucumber 81 - apple 82 - grape 83 - orange 84 - grape 85 - cherry 86 - grape 87 - cherry 88 - pear 89 - cherry 90 - apple 91 - kiwi 92 - banana 93 - kiwi 94 - banana 95 - melon 96 - banana 97 - melon 98 - pineapple 99 - apple 100 - pineapple ";
+
+//     function sortFruits(fruits) {
+//         let splitted = fruits.split(' ');
+//         let sorted = []
+//         for (let i = 0; i < splitted.length; i++) {
+//             let j = parseInt(splitted[i])
+//             if (splitted[i] !== "-" && splitted[i] !== NaN)
+//                 if (typeof j === 'number') {
+//                     sorted.push(splitted[i]);
+//                 }
+//         }
+//         console.log(sorted);
+//         sortedFruits = sorted;
+//     }
+//     sortFruits(fruits);
+
+//     function findNum(n) {
+//         let number = n;
+//         let arr = n.toString().split('');
+//         let sum = 0;
+//         if (number < 10) {
+//             FoundNumber = number
+//         } else if (number < 100) {
+//             number = number - (parseInt(arr[0]) + parseInt(arr[1]))
+//         } else {
+//             while (number > 100) {
+//                 for (let i = 0; i < arr.length; i++) {
+//                     sum += parseInt(arr[i]);
+//                 }
+//                 number -= sum;
+//                 arr = number.toString().split('');
+//                 sum = 0;
+//             }
+
+//             FoundNumber = number;
+
+//         }
+
+//         FoundNumber = number;
+//     }
+//     findNum(n);
+
+//     function findFruit(sorted, num) {
+//         for (let i = 0; i < sorted.length; i++) {
+//             console.log(sorted[i]);
+//             console.log(num);
+//             if (sorted[i] == num) {
+//                 return sorted[i + 1];
+//             }
+//         }
+//     }
+
+//     return findFruit(sortedFruits, FoundNumber);
+
+// };
+// console.log(SubtractSum());
