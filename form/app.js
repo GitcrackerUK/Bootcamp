9091a13b92dbb3cd7841e1e4b19c51c1faf4e3e5
gitcrackerUK <pfsiwek@gmail.com>
2020-05-22 20:58:20 +0100

    Form with object containing form values

--- form/app.js
+++ form/app.js
@@ -1,1 +1,23 @@
-alert('hello')
+// let form = document.querySelector('form')
+// const email = document.querySelector('#exampleInputEmail1');
+// const pass = document.querySelector('#exampleInputPassword1');
+// const check = document.querySelector('#exampleCheck1')
+// const sub = document.querySelector('#sub');
+// const formData = {}
+
+// form.addEventListener('submit', function (e) {
+//     console.log(e);
+//     submit()
+//     e.preventDefault()
+// })
+
+// const submit = function (e) {
+//     const emailVal = email.value;
+//     const passVal = pass.value;
+//     const checkVal = (check.checked) ? 'Yes' : 'No';
+//     console.log(`Email is:${emailVal} \nPass is: ${passVal} \nIs checked: ${checkVal}`)
+// }
+
+// email.addEventListener('input',(e)=>{formData['email']= e.target.value})
+// pass.addEventListener('input',(e)=> formData['password']=e.target.value)
+// check.addEventListener('input',(e)=> {formData['check']=e.target.checked })
