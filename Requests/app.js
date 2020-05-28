9fe6c6ef569e828cf72ecad3a36e1082838c0da6
gitcrackerUK <pfsiwek@gmail.com>
2020-05-28 15:55:18 +0100

    Axios request data to bootstrap table

--- Requests/app.js
+++ Requests/app.js
@@ -1,26 +1,60 @@
-const firstRequest = new XMLHttpRequest;
+// const firstRequest = new XMLHttpRequest;
 
-firstRequest.addEventListener('load',function(){
-	console.log('First works!!');
-	const response = JSON.parse(firstRequest.responseText)
-	console.log(response);
-	const responseURL = response.results[0].films[0]
+// firstRequest.addEventListener('load',function(){
+// 	console.log('First works!!');
+// 	const response = JSON.parse(firstRequest.responseText)
+// 	console.log(response);
+// 	const responseURL = response.results[0].films[0]
 
-	const secondRequest = new XMLHttpRequest;
-	secondRequest.addEventListener('load',function(){
-		console.log('Second works');
-		const response = JSON.parse(secondRequest.responseText)
-		console.log(response);
-	})
-	secondRequest.addEventListener('error',function(){
-		console.log('Second error');
-	})
-	secondRequest.open('GET', responseURL)
-	secondRequest.send()
+// 	const secondRequest = new XMLHttpRequest;
+// 	secondRequest.addEventListener('load',function(){
+// 		console.log('Second works');
+// 		const response = JSON.parse(secondRequest.responseText)
+// 		console.log(response);
+// 	})
+// 	secondRequest.addEventListener('error',function(){
+// 		console.log('Second error');
+// 	})
+// 	secondRequest.open('GET', responseURL)
+// 	secondRequest.send()
+// })
+// firstRequest.addEventListener('error',()=>{
+// 	console.log('Error!!!');
+// })
+// firstRequest.open('GET','http://swapi.dev/api/planets/')
+// firstRequest.send();
+
+// const prom = fetch('https://swapi.dev/api/planets/').then((res) => {
+// 	console.log(res)
+// 	let prom1 = res.json().then((res) => {
+// 		console.log(res.results[0].name);
+// 	}).catch((err)=> console.log(err))
+// 	console.log(prom1);
+// })
+// .catch((err)=>{
+// 	console.log(err);
+// })
+
+// console.log(prom);
+//  fetch('https://swapi.dev/api/people/1/').then((res)=>{
+// 	 console.log(res);
+//  })
+
+axios.get('https://swapi.dev/api/people').then((res)=>{
+	for(let person of res.data.results){
+		const tbody = document.querySelector('tbody')
+		const row = document.createElement('tr')
+		tbody.appendChild(row)
+		const th = document.createElement('th')
+		row.appendChild(th)
+		th.setAttribute('scope','row')
+		console.log(res.data.results);
+		let {name,gender,height,mass,eye_color} = person
+		const pers = {name,gender,height,mass,eye_color}
+		for(let item in pers){
+			const td = document.createElement('td')
+			row.appendChild(td)
+			td.innerText = pers[item]
+		}
+	}
 })
-firstRequest.addEventListener('error',()=>{
-	console.log('Error!!!');
-})
-firstRequest.open('GET','http://swapi.dev/api/planets/')
-firstRequest.send();
-
