9228f4bc71e20eaa71d3ce412c30ed86876fd290
GitcrackerUK <pfsiwek@gmail.com>
2025-06-23 21:17:15 +0100

    Added few types and data for better understandig

---
+++ ts/index.ts
@@ -0,0 +1,38 @@
+// TypeScript Basics
+
+// Primitives data types
+let age:number 
+let bread:string 
+let isActive:boolean 
+// Reference data types
+let daysOf:Array<Number>;
+type Garage = {
+    address: string;
+    numberOfCars: number;
+}
+
+type BlockOfFlats ={
+    name: string;
+    address: string;
+    numberOfFlats: number;  
+}
+let NewProject:BlockOfFlats = {
+    name: "Sunset Apartments",
+    address: "123 Ocean Drive",
+    numberOfFlats: 20
+};  
+
+let myGarage: Garage = {
+    address: "456 Car Street",
+    numberOfCars: 2
+};
+
+let NorthGarage: Garage = {
+    address : "New Passage Street",
+    numberOfCars: 5
+}
+let AvenueFlats:BlockOfFlats = {
+    name: "Avenue Flats",
+    address: "789 Avenue Road",
+    numberOfFlats: 10
+}
