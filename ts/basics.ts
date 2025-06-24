bd90c3ff74a69d8da71513c4b2ed7e018cd169a8
pawel <pfsiwek@gmail.com>
2025-06-24 10:32:59 +0100

    added .gitignore

---
+++ ts/basics.ts
@@ -0,0 +1,11 @@
+export let house: string;
+export let homeNumber: number;
+export let isHome: boolean;
+export let homeArray: Array<string>;
+export let adresArray: Array<number | string>;
+
+export const home2Array: Array<string> = ['home', 'street', 'city'];
+
+const dotCom:string = 'https://www.example.com';
+const reducer:boolean = true;
+
---
+++ ts/dist/basics.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.home2Array = exports.adresArray = exports.homeArray = exports.isHome = exports.homeNumber = exports.house = void 0;
+exports.home2Array = ['home', 'street', 'city'];
+const dotCom = 'https://www.example.com';
+const reducer = true;
