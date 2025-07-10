f8079cec34e581bd700f6a26e5a08f060ca7b9f6
GitcrackerUK <pfsiwek@gmail.com>
2025-07-10 14:29:02 +0100

    arrays and objects

--- ts/dist/index.js
+++ ts/dist/index.js
@@ -1,7 +1,10 @@
 "use strict";
+// TypeScript Basics
+// Primitives data types
 let age;
 let bread;
 let isActive;
+// Reference data types
 let daysOf;
 let NewProject = {
     name: 'Sunset Apartments',
@@ -32,10 +35,12 @@ let readingWeather = {
     windSpeed: 15,
     windDirection: 'North',
 };
+// In typeScript we can assign a value to it and the type will be assigned automatically.
 let ageGroup = 25;
 ageGroup = 30;
 let userName = 'Tony';
 userName = 'Trade';
+// To declare the function types:
 function declare(a, b) {
     return a + b;
 }
@@ -52,3 +57,71 @@ let sumMaster;
 sumMaster = sumOfTwo(sumWithInitialValue(1), sumWithInitialValue(3, 3));
 console.log(sumMaster);
 sumWithInitialValue(4);
+// To be able to assign all possible data type ANY can be used but it is missing the point of TS.
+let ageUser = 32; // any
+ageUser = '32';
+ageUser = false;
+ageUser = [];
+ageUser = {};
+//UNION
+// To be able to assign more than one type to variable like '34' and 34 string/number.
+// UNION type can be used with
+let newAge;
+// newAge = true; Throws an error
+newAge = 45;
+newAge = '33';
+let ar1;
+// Tuple
+ar1 = ['dog', 2];
+console.log(ar1[0], ar1[1]);
+// ar1 = ['cat',33,33] throw an error as tuple has only 2 valyes aigned but is passed 3
+// ar1 = [ 2, 'dog']; throw and error 
+console.log(ar1);
+let hobbies = ['sports', 'cooking'];
+hobbies.push('swimming');
+hobbies.pop();
+let users; // array which accepts only strings.
+let dates; // array which accepts strings and numbers.
+//To use this way of defining type variable need to be assign with square brackets first and not with .push or other methods.
+users = [];
+dates = ['20/2/22', 33];
+// After assigning square brackets and maybe even data methods like .push can be used.
+users.push('John');
+let weather;
+let possibleResults;
+//Objects
+let standardOBJ = {
+    name: 'John',
+    surname: 'Smith'
+};
+// Defining types.
+let cats = {
+    name: 'meow',
+    age: '4',
+    hobbies: ['swimming', 'jumping', 'painting'],
+    home: {
+        street: 'Hardwick Road',
+        homeNumber: 45,
+    },
+};
+// NON NULL and NON UNDEFINED value
+let nonEmpty;
+nonEmpty = ['cat', 4, null, undefined];
+console.log(nonEmpty); // Shows array with null and undefined.
+// nonEmpty = null; It throws an error as null is assigned directly to the variable which has {}.
+nonEmpty = 34;
+nonEmpty = '33';
+// nonEmpty = undefined; Throws an error.
+let employee;
+employee = {
+    name: 'John',
+    surname: 'Thompson',
+    address: {
+        street: 'Niceone street',
+        houseNumber: '33',
+        city: 'Cardiff'
+    },
+    age: '45',
+    hobbies: ['swimming', 'slipping', 'sitting']
+};
+console.log(employee);
--- ts/index.ts
+++ ts/index.ts
@@ -104,8 +104,98 @@ ageUser = {};
 
 let newAge: string | number;
 
-newAge = true; // Throws an error
+// newAge = true; Throws an error
 newAge = 45;
 newAge = '33';
 
+let ar1: [string,number];
 
+
+// Tuple
+ar1 = ['dog',2];
+console.log(ar1[0],ar1[1]);
+
+// ar1 = ['cat',33,33] throw an error as tuple has only 2 valyes aigned but is passed 3
+// ar1 = [ 2, 'dog']; throw and error 
+console.log(ar1);
+
+let hobbies = ['sports','cooking'];
+hobbies.push('swimming');
+hobbies.pop();
+
+
+let users:string[]; // array which accepts only strings.
+let dates:(string|number)[]; // array which accepts strings and numbers.
+//To use this way of defining type variable need to be assign with square brackets first and not with .push or other methods.
+users = []
+dates = ['20/2/22',33]
+// After assigning square brackets and maybe even data methods like .push can be used.
+users.push('John');
+
+let weather:Array<string|number>;
+
+let possibleResults:[string,number]
+
+//Objects
+
+let standardOBJ = {
+	name:'John',
+	surname:'Smith'
+}
+
+// Defining types.
+let cats:{
+	name:string;
+	age:number|string;
+	home:{
+		street: string,
+		homeNumber: number;
+	};
+	hobbies:string[]
+} = {
+	name:'meow',
+	age:'4',
+	hobbies:['swimming','jumping','painting'],
+	home:{
+		street: 'Hardwick Road',
+		homeNumber: 45,
+	},
+}
+// NON NULL and NON UNDEFINED value
+
+let nonEmpty:{};
+
+nonEmpty = ['cat',4,null,undefined];
+console.log(nonEmpty); // Shows array with null and undefined.
+
+// nonEmpty = null; It throws an error as null is assigned directly to the variable which has {}.
+
+nonEmpty = 34;
+nonEmpty = '33';
+// nonEmpty = undefined; Throws an error.
+
+let employee:{
+	name:string,
+	surname:string,
+	address:{
+		street:string,
+		houseNumber:string|number,
+		city:string
+	}
+	age: string|number,
+	hobbies:string[]
+};
+
+employee = {
+	name:'John',
+	surname:'Thompson',
+	address:{
+		street:'Niceone street',
+		houseNumber:'33',
+		city:'Cardiff'
+	},
+	age:'45',
+	hobbies:['swimming','slipping','sitting']
+}
+
+console.log(employee);
